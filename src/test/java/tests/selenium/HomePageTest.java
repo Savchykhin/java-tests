d03ca7f51123f0c6f846ab9e771d81ebb70091dd
OLeksandr <savchykhin@gmail.com>
2023-05-14 13:24:13 -0700

    selenium setup

--- src/test/java/tests/selenium/HomePageTest.java
+++ src/test/java/tests/selenium/HomePageTest.java
@@ -2,23 +2,15 @@ package tests.selenium;
 
 import org.assertj.core.api.Assertions;
 import org.openqa.selenium.By;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
-
 
 public class HomePageTest extends TestController {
 
     @Test
     public void homePage() {
-        //Optional, setup firefox driver
-        //System.setProperty("webdriver.gecko.driver","/path/to/webdriver");
-        driver = new FirefoxDriver();
-        driver.get("http://localhost:8000");
-
         Assertions.assertThat(driver.getTitle()).isEqualTo("Employee CRUD");
         Assertions.assertThat(driver.findElement(By.cssSelector(".navbar-header > h1")).getText())
                 .isEqualTo("Welcome, Sample App");
-
     }
 
 }
--- src/test/java/tests/selenium/TestController.java
+++ src/test/java/tests/selenium/TestController.java
@@ -1,11 +1,22 @@
 package tests.selenium;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 
 public class TestController {
 
-    WebDriver driver;
+    protected WebDriver driver;
+
+    @BeforeTest
+    public void setUp() {
+         //Optional, setup firefox driver
+         System.setProperty("webdriver.gecko.driver","/Users/alex/Downloads/employee-crud/bin/geckodriver");
+         driver = new FirefoxDriver();
+         driver.get("http://localhost:8000");
+    }
+
 
     @AfterTest
     public void closeDriver() {
